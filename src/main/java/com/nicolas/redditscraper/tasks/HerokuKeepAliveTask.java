52ec17d03166b09a5f8c281dfa5c00f6e2b35622
nicolasmoura <nicolas-md@hotmail.com>
2018-01-17 08:49:19 -0200

    adicionando schedule de keepalive p/ heroku do front end

--- src/main/java/com/nicolas/redditscraper/tasks/HerokuKeepAliveTask.java
+++ src/main/java/com/nicolas/redditscraper/tasks/HerokuKeepAliveTask.java
@@ -10,6 +10,7 @@ public class HerokuKeepAliveTask {
     @Scheduled(fixedRate = 300000)
     public void keepAlive() {
         try {
+            Unirest.get("https://rddt-scraper.herokuapp.com").asString();
             Unirest.get("https://rddt-scraper-api.herokuapp.com").asString();
             System.out.println("Sending keepAlive request to Heroku: DONE");
         } catch (UnirestException e) {
